d03519ecd631bc7f8527e871790d2a701257a57d
wahidkhan05 <107915922+wahidkhan05@users.noreply.github.com>
2023-03-10 00:47:59 +0530

    Update webapp_with_streamlit_for_deployment_live.py

--- webapp_with_streamlit_for_deployment_live.py
+++ webapp_with_streamlit_for_deployment_live.py
@@ -1,35 +1,30 @@
-import numpy as np
-import pandas as pd
 import pickle
 import streamlit as st
-# from sklearn.preprocessing import StandardScaler
 from streamlit_option_menu import option_menu
-
+ 
 
 loaded_model = pickle.load(open("trained_model.sav",'rb'))
 
-def diabetes_prediction(input_data):
-    # scaler = StandardScaler()
+with st.sidebar:
 
-    input_data_as_np_array = np.asarray(input_data)
+    selected = option_menu("Multiple Disease Predictive system",
+                           ['Diabetes Prediction'
+                            ],
+                           icons = ['activity'],
+                           default_index=0)
 
-    input_data_reshaped = input_data_as_np_array.reshape(1, -1)
+# icons name have to be taken from bootstrap
+
+if (selected =='Diabetes Prediction'):
+    # giving a title
+    st.title('Diabetes Prediction using ML')
+
+
+
 
     # std_data = scaler.transform(input_data_reshaped)
     # print(std_data)
 
-    prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
-
-    if prediction[0] == 0:
-        return 'person is not diabetic'
-    else:
-        return 'person is diabetic'
-
-def main():
-
-    # giving a title
-    st.title('Diabetes prediction web app')
 
     # getting the input data from user
     # columns for input fields :
@@ -52,10 +47,6 @@ def main():
     with col2:
         Age = st.text_input('Age')
 
-
-
-
-
     # code for prediction
 
     diagnosis = ''
@@ -63,27 +54,14 @@ def main():
     # creating a button for prediction
 
     if st.button('Diabetes test result'):
-        diagnosis = diabetes_prediction([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+        diagnosis = loaded_model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+
+
+        if (diagnosis[0] == 1):
+            diagnosis = 'person is diabetic'
+        else:
+            diagnosis = 'person is not diabetic'
 
     st.success(diagnosis)
-
-if __name__ == '__main__':
-    main()
-
-with st.sidebar:
-
-    selected = option_menu("Multiple Disease Predictive system",
-                           ['Diabetes Prediction'
-                            ],
-                           icons = ['activity'],
-                           default_index=0)
-
-# icons name have to be taken from bootstrap
-
-if (selected =='Diabetes Prediction'):
-
-    st.title('Diabetes Prediction using ML')
-
-
 # streamlit run "filepath"
 
